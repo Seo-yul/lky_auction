12036adc7b0377c32bc1994f17e9b9e7545e5e60
dev-yujin <kyouj96@gmail.com>
2020-11-01 19:22:50 +0900

    model design , no migrate

--- lky/auction/models.py
+++ lky/auction/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    # 상품명
+    name = models.CharField(max_length=60, default='')
+    # 사진 -> media file 설정 해주기
+    # photo = models.ImageField(upload_to='images/',blank=True, null=True)
+    # 글 내용
+    content = models.TextField()
+    # 판매자 아이디 -> user/models.py/User 상속받기?
+    # seller_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    
+    # 최소가격
+    min_price = models.IntegerField(default=0)
+    # 현재가격 (최대가격)
+    max_price = models.IntegerField(default=0)
+    
+    # 시작일
+    start_date = models.DateField()
+    # 마감일
+    end_date = models.DateField()
+    # 게시글 등록 날짜
+    pub_date = models.DateTimeField(auto_now_add=True)
+    # 대분류
+    category = models.CharField(max_length=60, default='')
+    
+    def __str__(self):
+        return self.name
--- lky/user/models.py
+++ lky/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # 아이디
+    user_id = models.CharField(primary_key=True, max_length=60, default='')
+    # 비밀번호
+    user_pwd = models.CharField(max_length=60, default='' )
+    # 이름
+    user_name = models.CharField(max_length=60, default='')
+    # 가입일 : 저장되는 시점의 시간을 자동으로 등록
+    join_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user_name

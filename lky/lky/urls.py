15f1e2d160459c5828bf00823d187396e0e4d220
miniongo3o <dlalsdyd094@naver.com>
2020-11-02 22:39:18 +0900

    #2 부트스트랩 적용, index 페이지

--- lky/lky/urls.py
+++ lky/lky/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/', include('auction.urls')),
-    # path('api/', include('user.urls')),
+    path('', include('user.urls')),
 ]

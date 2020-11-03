1757750250bfb196f3bebbc32059c25d1136b8f0
eunseo <youesdia0704@gmail.com>
2020-11-03 17:36:14 +0900

    make base.html

--- lky/auction/views.py
+++ lky/auction/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'auction/index.html')
+
+def auctionRegister(request):
+    return render(request, 'auction/auction_register.html')

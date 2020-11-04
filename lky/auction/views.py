f351b495655cb704dd101142bbbfd1d648a64803
Eunseo Yu <youesdia0704@gmail.com>
2020-11-04 16:05:48 +0900

    product list w/o image

--- lky/auction/views.py
+++ lky/auction/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render,redirect
 from .forms import registerForm#
-# Create your views here.
+from .models import Product
+
+
 def index(request):
-    return render(request, 'auction/index.html')
+    product = Product.objects.all().order_by('-pub_date')[:6]
+    return render(request, 'auction/index.html', {'product': product})
+
 
 def auctionRegister(request):
-    #return render(request, 'auction/auction_register.html')
+    # return render(request, 'auction/auction_register.html')
     if request.method == 'POST':
         form = registerForm(request.POST)
 
         if form.is_valid():
             prod = form.save(commit=False)
-            #prod.author = request.user
-            #prod.published_date = timezone.now()
+            # prod.author = request.user
+            # prod.published_date = timezone.now()
             prod.save()
-
 
             return redirect('index')
     else:
         form = registerForm()
 
     return render(request, 'auction/auction_register.html', {'form': form})
+
+
+# def showProductList(request):
+#     product = Product
+#     return render(request, product)

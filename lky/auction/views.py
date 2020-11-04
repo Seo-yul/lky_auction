2e1c470a1964da12279cc5f5eb6a6a0be4473c5f
miniongo3o <dlalsdyd094@naver.com>
2020-11-04 11:46:01 +0900

    #2 connect base-login/sign up,  product register

--- lky/auction/views.py
+++ lky/auction/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import registerForm#
 # Create your views here.
 def index(request):
     return render(request, 'auction/index.html')
 
 def auctionRegister(request):
-    return render(request, 'auction/auction_register.html')
+    #return render(request, 'auction/auction_register.html')
+    if request.method == 'POST':
+        form = registerForm(request.POST)
+
+        if form.is_valid():
+            prod = form.save(commit=False)
+            #prod.author = request.user
+            #prod.published_date = timezone.now()
+            prod.save()
+
+
+            return redirect('index')
+    else:
+        form = registerForm()
+
+    return render(request, 'auction/auction_register.html', {'form': form})

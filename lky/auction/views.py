73214c4eaf57e92c5f0ce88e8964c7220d7e2d02
miniongo3o <dlalsdyd094@naver.com>
2020-11-04 18:25:14 +0900

    #2 charging credit, extract user model

--- lky/auction/views.py
+++ lky/auction/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import registerForm
+from .models import Product
+from django.contrib.auth.models import User
+from user.models import My_user
+
 # Create your views here.
 def index(request):
     return render(request, 'auction/index.html')
@@ -23,3 +27,21 @@ def auctionRegister(request):
         form = registerForm()
 
     return render(request, 'auction/auction_register.html', {'form': form})
+
+
+# 홈 상단 바 - 크레딧 충전 클릭
+def auction_credit(request):
+    return render(request,'auction/auction_credit.html')
+
+
+# auction_credit.html에서 실행할 충전 함수
+# 현재 로그인 중인 auth_user의 id에 해당하는 크레딧을 50000원 증가시킨다.
+def charging(request):
+    user_id=request.user
+    print(user_id.id)
+
+    now_credit=My_user.objects.get(user_id=user_id.id)
+    now_credit.credit=int(now_credit.credit)+50000
+    now_credit.save()
+    return render(request,'auction/auction_credit.html')
+
--- lky/user/views.py
+++ lky/user/views.py
@@ -3,13 +3,16 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User # 장고가 주는 User 모델 
 from django.contrib import auth
-
+from .models import My_user
 def signup(request):
     if request.method == "POST":
         if request.POST["password1"] == request.POST["password2"]: # 비밀번호가 같으면 회원가입
             user = User.objects.create_user(
                 username=request.POST["username"],password=request.POST["password1"]
             )
+
+            my_user=My_user(user=user,credit=0)
+            my_user.save()
             auth.login(request, user)
             return redirect('login')
         return render(request, 'user/signup.html')
@@ -17,7 +20,7 @@ def signup(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST["username"]
+        username = request.POST["your_name"]
         password = request.POST["password"]
         user = auth.authenticate(request, username=username, password=password) 
         if user is not None: # 유저가 존재하면 이전페이지로?
@@ -25,7 +28,7 @@ def login(request):
             return HttpResponseRedirect('/')
             # return render(request, 'auction/index.html')
         else: # 없으면 로그인 페이지를 렌더
-            return render(request, 'login.html', {'error':'username or password is incorrect'})
+            return render(request, 'user/login.html', {'error':'username or password is incorrect'})
             # 로그인 실패시 'username or password is incorrect' 메시지를 띄움  
     else:
         return render(request, 'user/login.html')
